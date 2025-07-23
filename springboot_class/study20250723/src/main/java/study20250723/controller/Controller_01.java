c8411e800f0db7767ee9e00a8d1a34fccabc52b9
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-23 11:22:25 +0900

    controller 추가

---
+++ springboot_class/study20250723/src/main/java/study20250723/controller/Controller_01.java
@@ -0,0 +1,10 @@
+package study20250723.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Controller_01 {
+    public String method01() {
+        return "hi";
+    }
+}
