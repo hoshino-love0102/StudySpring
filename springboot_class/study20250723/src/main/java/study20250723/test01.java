7339326623bd89a166e4ff51f3acdb36e16a0f6e
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-23 10:47:08 +0900

    기본설명 +

--- springboot_class/study20250723/src/main/java/study20250723/test01.java
+++ springboot_class/study20250723/src/main/java/study20250723/test01.java
@@ -1,9 +1,11 @@
 package study20250723;
 
 public class test01 {
-    // 외부 라이브러리
-    // spring web 스프링 웹서버를 만들어 주기 위한 기본 도구
-    // H2 DB 데이터베이스 (mysql, mariaDB, mssql, oracle)
-    // Mustche, Thymeleaf - 화면을 만들어 주는놈, 뷰 템플릿
-    // JPA - 데이터베이스를 관리해주는 놈 (열라 편함)
+    // localhost:8080
+    // 아이디:포트, DNS
+    // 대구시 달서구 용산동(도메인) -> GPS좌표(아이피)
+    // 아이피는 주소 (정확한 위치)
+    // 포트: 몇층
+    // 컨트롤러 : 안내인(니는 어디로가)
+    // 프로토콜: http, ssh, ftp
 }
