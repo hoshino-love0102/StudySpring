535b8142ef472475b601ed7d1ff84cc2b3c339ee
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-23 11:27:45 +0900

    GetMapping으로 hi사용

--- springboot_class/study20250723/src/main/java/study20250723/controller/Controller_01.java
+++ springboot_class/study20250723/src/main/java/study20250723/controller/Controller_01.java
@@ -1,9 +1,11 @@
 package study20250723.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Controller_01 {
+    @GetMapping("/hi")
     public String method01() {
         return "hi";
     }
