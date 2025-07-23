54d182bb379d6f22ecce7ff3617cbb0b3809cc8b
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-23 11:56:36 +0900

    username추가

--- springboot_class/study20250723/src/main/java/study20250723/controller/Controller_01.java
+++ springboot_class/study20250723/src/main/java/study20250723/controller/Controller_01.java
@@ -1,12 +1,14 @@
 package study20250723.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Controller_01 {
-    @GetMapping("/hi")
-    public String method01() {
+    @GetMapping("/abcd")
+    public String method01(Model model) {
+        model.addAttribute("username", "admin");
         return "hi";
     }
 }
