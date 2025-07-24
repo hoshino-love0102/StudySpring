acd418827ae427ebebc4fac10bd138b49784ac0d
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-24 15:51:43 +0900

    불필요한 import삭제

--- NoteProject/src/main/java/com/example/noteproject/service/NoteService.java
+++ NoteProject/src/main/java/com/example/noteproject/service/NoteService.java
@@ -6,8 +6,6 @@ import com.example.noteproject.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class NoteService {
 
---
+++ loginproject/src/main/java/com/example/loginproject/controller/PageController.java
@@ -0,0 +1,4 @@
+package com.example.loginproject.controller;
+
+public class PageController {
+}
