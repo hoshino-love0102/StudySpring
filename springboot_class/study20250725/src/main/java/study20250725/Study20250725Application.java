98777c00cd185c906aafe2733040f13a0b992ec1
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-25 09:48:19 +0900

    기본 설정

---
+++ springboot_class/study20250725/src/main/java/study20250725/Study20250725Application.java
@@ -0,0 +1,13 @@
+package study20250725;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study20250725Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Study20250725Application.class, args);
+    }
+
+}
---
+++ springboot_class/study20250725/src/test/java/study20250725/Study20250725ApplicationTests.java
@@ -0,0 +1,13 @@
+package study20250725;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Study20250725ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
