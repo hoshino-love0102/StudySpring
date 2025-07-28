4165da8d38bfd5c2290deaae7bb897667e173290
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-28 14:02:18 +0900

    Add MemberDto

--- springboot_class/study20250728/src/main/java/javastudy20250728/dto/MemberDto.java
+++ springboot_class/study20250728/src/main/java/javastudy20250728/dto/MemberDto.java
@@ -1,4 +1,23 @@
 package javastudy20250728.dto;
 
+import javastudy20250728.entity.Member;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MemberDto {
+    private Long id;
+    private String name;
+    private String email;
+
+    public static MemberDto fromEntity(Member member) {
+        return MemberDto.builder()
+                .id(member.getId())
+                .name(member.getName())
+                .email(member.getEmail())
+                .build();
+    }
 }
