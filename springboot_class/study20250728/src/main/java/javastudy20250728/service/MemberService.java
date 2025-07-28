8a1a4d2eff13a096614fc0b6d0c33a6f0e84f242
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-28 14:04:50 +0900

    add MemberService

--- springboot_class/study20250728/src/main/java/javastudy20250728/service/MemberService.java
+++ springboot_class/study20250728/src/main/java/javastudy20250728/service/MemberService.java
@@ -1,4 +1,43 @@
 package javastudy20250728.service;
 
+import javastudy20250728.dto.MemberDto;
+import javastudy20250728.entity.Member;
+import javastudy20250728.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public List<MemberDto> findAll() {
+        return memberRepository.findAll()
+                .stream()
+                .map(MemberDto::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    public MemberDto findById(Long id) {
+        Member member = memberRepository.findById(id).orElseThrow();
+        return MemberDto.fromEntity(member);
+    }
+
+    public void save(MemberDto dto) {
+        Member member = Member.builder()
+                .name(dto.getName())
+                .email(dto.getEmail())
+                .build();
+        memberRepository.save(member);
+    }
+
+    public long countMembers() {
+        return memberRepository.count();
+    }
+
 }
