9e7b88d7fdef6b3acf074941660b6929d3ea2b1f
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-28 14:05:13 +0900

    mustache파일 업로드

---
+++ springboot_class/study20250728/src/main/java/javastudy20250728/Study20250728Application.java
@@ -0,0 +1,13 @@
+package javastudy20250728;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study20250728Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Study20250728Application.class, args);
+    }
+
+}
---
+++ springboot_class/study20250728/src/test/java/javastudy20250728/Study20250728ApplicationTests.java
@@ -0,0 +1,13 @@
+package javastudy20250728;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Study20250728ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
