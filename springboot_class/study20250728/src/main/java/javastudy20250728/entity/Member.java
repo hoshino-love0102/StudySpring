2c3fdd13342f604d55d772c08fbfeeb4a928ec04
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-28 14:03:05 +0900

    add Memeber entity

--- springboot_class/study20250728/src/main/java/javastudy20250728/entity/Member.java
+++ springboot_class/study20250728/src/main/java/javastudy20250728/entity/Member.java
@@ -1,4 +1,19 @@
 package javastudy20250728.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
 }
